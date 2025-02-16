e5d19c7b0cbd72e19a1d4e822e6a4b58c5ce8e0f
Airstriker <amit/singh12@education.nsw.gov.au>
2025-02-16 22:35:21 +1100

    added yt vid downloaded mp3/mp4

--- appdata/app_functions/youtube to mp3 or mp4.py
+++ appdata/app_functions/youtube to mp3 or mp4.py
@@ -7,6 +7,7 @@ https://www.youtube.com/watch?v=VgxnyKnB3qc
 https://www.youtube.com/watch?v=ucXTQ0V8qMA&t=164s
 
 '''
+#autmatic?
 #pip install pytube pydub ffmpeg
 # pip install pytubefix
 #pip install imageio[ffmpeg]
@@ -16,7 +17,7 @@ from pydub import AudioSegment
 from colors_app import *
 import os
 
-# Create the folder if it doesn't exist
+
 download_folder = "downloaded content"
 os.makedirs(download_folder, exist_ok=True)
 
@@ -34,23 +35,21 @@ def mp4():
 def mp3():
     yt = YouTube(url, on_progress_callback=on_progress)
     print(f"Downloading Audio: {yt.title}")
-
-    # Filter for audio-only streams (but these will usually be in M4A)
     stream = yt.streams.filter(only_audio=True).first()
 
     if not stream:
         print(f"{red}No audio stream available.")
         return
 
-    # Download the file (this will usually be in M4A format)
+    # this will usually be in M4A format
     temp_file = stream.download(download_folder)
     mp3_file = os.path.join(download_folder, os.path.splitext(os.path.basename(temp_file))[0] + ".mp3")
 
-    # Convert M4A to MP3 using pydub
+    # Convert M4A to MP3 using pydub ;0
     try:
-        audio = AudioSegment.from_file(temp_file, format="m4a")  # Explicitly specify the format as M4A
+        audio = AudioSegment.from_file(temp_file, format="m4a")  # this prob does not work ;/
         audio.export(mp3_file, format="mp3")
-        os.remove(temp_file)  # Clean up the original M4A file
+        os.remove(temp_file)  # delete useless file ewwwwwwwwwwww -_-
         print(f"{green}MP3 downloaded! Check the {red}'downloaded content' folder.")
     except Exception as e:
         print(f"{red}Error during conversion: {e}")
