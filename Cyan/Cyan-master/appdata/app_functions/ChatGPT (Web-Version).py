ea0f6acef7a4c5c1c0e0fcefe5bdd63971bc6631
Airstriker <amit/singh12@education.nsw.gov.au>
2025-02-17 22:21:53 +1100

    Working app! (fixed setup bug)

--- Cyan/Cyan-master/appdata/app_functions/ChatGPT (Web-Version).py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-from colors_app import *
-
-print(f'{red}starting webbroswer{cyan}:{white} chatgpt')
-webbrowser.open('chatgpt.com')
--- start.bat
+++ start.bat
@@ -1,8 +1,14 @@
 @echo off
 color 2
-echo Installing Python modules:
-pip install -r appdata\requirements.txt
+echo Installing Python modules...
+pip install --upgrade pip
+pip install -r appdata/requirements.txt
+if %errorlevel% neq 0 (
+    echo Error installing modules. Check errors above.
+    pause
+    exit /b
+)
 color 1
-echo starting app (cyan.py)
+echo Starting app (cyan.py)
 python cyan.py
 pause
